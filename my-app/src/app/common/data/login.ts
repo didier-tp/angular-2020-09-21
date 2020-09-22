50357230b482696c0c3f429c8fea2cba744e69b9
didier <didier@d-defrance.fr>
2020-09-22 12:34:12 +0200

    login

---
+++ my-app/src/app/common/data/login.ts
@@ -0,0 +1,5 @@
+export class Login {
+    constructor(public username : string = undefined ,
+                public password : string = undefined ,
+                public roles : string = undefined  ) { }
+}
---
+++ my-app/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Login } from '../common/data/login';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  login : Login = new Login();
+
+  message : string; 
+
+  onLogin(){
+   this.message = "donnees saisies = " + JSON.stringify(this.login);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

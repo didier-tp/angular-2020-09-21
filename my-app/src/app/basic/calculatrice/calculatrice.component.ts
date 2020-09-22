ede9094b82dac8c51db1acc4be042d9fe7138073
didier <didier@d-defrance.fr>
2020-09-22 11:01:22 +0200

    calculatrice

--- my-app/src/app/basic/calculatrice/calculatrice.component.ts
+++ my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -6,19 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculatrice.component.scss']
 })
 export class CalculatriceComponent implements OnInit {
-
+/*
+  dataCalcul = {
+    x : 0 ,
+    y : 0 ,
+    res : 0
+  };
+*/
+  
   x : number = 0;
   y : number =1;
 
   res : number;
 
+
   onAdditionner() :void{
     //this.res=this.x + this.y;
      this.res=Number(this.x)+Number(this.y);  //Number() avec un N majuscule est fonction prédéfinie de javascript
+    //this.dataCalcul.res = Number(this.dataCalcul.x) + Number(this.dataCalcul.y);
   }
 
   onMultiplier() :void{
     this.res=this.x * this.y;
+   //this.dataCalcul.res = this.dataCalcul.x * this.dataCalcul.y;
  }
 
   constructor() { }

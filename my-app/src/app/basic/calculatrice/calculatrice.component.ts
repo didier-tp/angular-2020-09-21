a8c38a781a596bbd60da4a35932b6d1bd6c28a9b
didier <didier@d-defrance.fr>
2020-09-22 10:27:36 +0200

    s

--- my-app/src/app/basic/calculatrice/calculatrice.component.ts
+++ my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatriceComponent implements OnInit {
 
+  x : number = 0;
+  y : number =1;
+
+  res : number;
+
+  onAdditionner() :void{
+    //this.res=this.x + this.y;
+     this.res=Number(this.x)+Number(this.y);  //Number() avec un N majuscule est fonction prédéfinie de javascript
+  }
+
+  onMultiplier() :void{
+    this.res=this.x * this.y;
+ }
+
   constructor() { }
 
   ngOnInit(): void {

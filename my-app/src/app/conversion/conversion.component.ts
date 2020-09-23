7cf215117458cd0c5d5f017dd457f68dbaf4f602
didier <didier@d-defrance.fr>
2020-09-23 11:46:30 +0200

    s

--- my-app/src/app/conversion/conversion.component.ts
+++ my-app/src/app/conversion/conversion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviseService } from '../common/service/devise.service';
+import { Devise } from '../common/data/devise';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-conversion',
@@ -8,7 +10,20 @@ import { DeviseService } from '../common/service/devise.service';
 })
 export class ConversionComponent implements OnInit {
 
-  constructor(private deviseService : DeviseService) { }
+  montant : number = 100;
+  codeDevSource : string = "EUR";
+  codeDevCible : string = "USD";
+  montantConverti : number ;
+
+  listeDevises : Devise[]; 
+
+  constructor(private deviseService : DeviseService) {
+      this.listeDevises = deviseService.getAllDevises();
+   }
+
+   onConversion(){
+     this.montantConverti =   this.deviseService.convertir(this.montant , this.codeDevSource , this.codeDevCible );
+   }
 
   ngOnInit(): void {
   }

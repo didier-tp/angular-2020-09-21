19c1955a9c82129f019f6b41ea215351f39182dc
didier <didier@d-defrance.fr>
2020-09-23 14:47:30 +0200

    s

--- my-app/src/app/common/service/devise.service.ts
+++ my-app/src/app/common/service/devise.service.ts
@@ -4,6 +4,16 @@ import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
+interface ResConv {
+  source : string;
+  target : string;
+  amount : number;
+  result: number;
+}
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,14 +24,14 @@ export class DeviseService {
   // ancienne pré-version a (simulation , sans asynchrone):
   //pré-version b (simulation , avec asynchronisme et Observable)
   //réelle version c (appel de WS REST)
-
+/*
   private listeDevises = [
     { code : "EUR" , name : "Euro" , change : 1.0 } ,
     { code : "USD" , name : "Dollar" , change : 1.1 } ,
     { code : "GBP" , name : "Livre" , change : 0.9 } ,
     { code : "JPY" , name : "Yen" , change : 120.0 } 
   ];
-
+*/
   public getAllDevises() : Observable < Devise[] > {
        //return of(this.listeDevises);
        let wsUrl = "http://localhost:8282/devise-api/public/devise";
@@ -30,12 +40,13 @@ export class DeviseService {
 
   public convertir(montant : number, codeDevSource : string , codeDevCible : string) : Observable< number >{
     // return of(montant * 1.1234); //simulation rapide   
-        let wsUrl = "http://localhost:8282/devise-api/public/convert?source="
-                   +codeDevSource+"&target=" +codeDevCible + "&amount=" + montant ;
+       /* let wsUrl = "http://localhost:8282/devise-api/public/convert?source="
+                   +codeDevSource+"&target=" +codeDevCible + "&amount=" + montant ; */
+      let wsUrl = `http://localhost:8282/devise-api/public/convert?source=${codeDevSource}&target=${codeDevCible}&amount=${montant}` ;
        
-       return this.http.get<object>(wsUrl)
+       return this.http.get<ResConv>(wsUrl)
                        .pipe(
-                           map( (resConv) => resConv['result'] )
+                           map( (resConv : ResConv) => resConv.result )
                        );
   }
 }

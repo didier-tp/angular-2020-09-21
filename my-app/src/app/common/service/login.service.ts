35f413e7aa24c65aab2973b200595ac496399574
didier <didier@d-defrance.fr>
2020-09-23 17:09:02 +0200

    s

--- my-app/src/app/common/service/login.service.ts
+++ my-app/src/app/common/service/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Login } from '../data/login';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators'
 import { LoginResponse } from '../data/loginResponse';
 
 @Injectable({
@@ -15,6 +16,9 @@ export class LoginService {
 
   public postLogin(login: Login): Observable<LoginResponse>{
        let wsUrl ="http://localhost:8282/login-api/public/auth";
-       return this.http.post<LoginResponse>(wsUrl,login, { headers : this._headers});
+       return this.http.post<LoginResponse>(wsUrl,login, { headers : this._headers})
+                   .pipe(
+                     tap( (loginResponse : LoginResponse)=> { localStorage.setItem('token',loginResponse.token) })
+                   );
   }
 }

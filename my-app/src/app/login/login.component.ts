3d885e17aeb6540ced86fb93b6be4578480ab4d9
didier <didier@d-defrance.fr>
2020-09-23 16:38:58 +0200

    post

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Login } from '../common/data/login';
 import { LoginService } from '../common/service/login.service';
+import { LoginResponse } from '../common/data/loginResponse';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,17 @@ import { LoginService } from '../common/service/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  login : Login = new Login();
+  login : Login = new Login("admin1","pwdadmin1","admin");
 
   message : string; 
 
   onLogin(){
    this.message = "donnees saisies = " + JSON.stringify(this.login);
+   this.loginService.postLogin(this.login)
+                    .subscribe(
+                      (loginResponse : LoginResponse)=>{  this.message = JSON.stringify(loginResponse);     },
+                      (error)=> { console.log(error); this.message ="erreur ..."}
+                    );
   }
 
   constructor(private loginService : LoginService) { }

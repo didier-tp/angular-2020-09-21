1cca751c10c6e60d1523260cdf021329ec0224bc
didier <didier@d-defrance.fr>
2020-09-29 10:55:14 +0200

    validator

--- my-app/src/app/common/validators/roles.validator.ts
+++ my-app/src/app/common/validators/roles.validator.ts
@@ -1,7 +1,11 @@
 import { AbstractControl } from '@angular/forms';
 export function ValidateRole(control: AbstractControl) {
-    if (!control.value.startsWith('admin')  ) {
-    return { invalidRole: true }; //return { errorKeyname : true } if invalid
+    if (control.value.startsWith('admin') ||  control.value.startsWith('user')
+    || control.value.startsWith('publisher') ) {
+        return null; //return null if ok (no error)
     }
-    return null; //return null if ok (no error)
+    else{
+        return { invalidRole: true }; //return { errorKeyname : true } if invalid
+    }
+    
 }

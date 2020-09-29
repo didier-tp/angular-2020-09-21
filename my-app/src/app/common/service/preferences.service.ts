1939974d3734460e87e094e04813a5722cfbb3f1
didier <didier@d-defrance.fr>
2020-09-29 15:23:11 +0200

    s

--- my-app/src/app/common/service/preferences.service.ts
+++ my-app/src/app/common/service/preferences.service.ts
@@ -8,22 +8,22 @@ export class PreferencesService {
 
   private _couleurFondPreferee : string = "white"; //valeur par defaut
 
-  private _bsCouleutFondPreferee : BehaviorSubject<string>; //initailisation dans constructeur
+  private _bsCouleurFondPreferee : BehaviorSubject<string>; //initailisation dans constructeur
 
   //public get couleurFondPreferee() { return this._couleurFondPreferee; }
-  public get couleurFondPrefereeObs() { return this._bsCouleutFondPreferee; }
+  public get couleurFondPrefereeObs() { return this._bsCouleurFondPreferee; }
 
   //public set couleurFondPreferee(c : string) {
   public setCouleurFondPreferee(c : string) {
      this._couleurFondPreferee = c; 
-     this._bsCouleutFondPreferee.next(c); //NB: next(nouvelleValeur) declencle toutes les callbacks enregistrees via .subscribe()
-                                          //sur ce meme buhaviorSubject (cas particulier d'Observable)
+     this._bsCouleurFondPreferee.next(c); //NB: next(nouvelleValeur) declencle toutes les callbacks enregistrees via .subscribe()
+                                          //sur ce meme behaviorSubject (cas particulier d'Observable)
      localStorage.setItem("couleurFondPreferee",this._couleurFondPreferee);
     }
 
   constructor() { 
     let c = localStorage.getItem("couleurFondPreferee");
     this._couleurFondPreferee = c?c:'white' ;
-    this._bsCouleutFondPreferee  = new BehaviorSubject<string>(this._couleurFondPreferee);
+    this._bsCouleurFondPreferee  = new BehaviorSubject<string>(this._couleurFondPreferee);
   }
 }
